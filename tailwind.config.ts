f7f47c358a0d02c8e37102ed21044b8c6e9fad78
Le-Jr <nascimentoleandrojr@gmail.com>
2025-05-29 13:19:36 -0300

    feat: font config

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        apple: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
