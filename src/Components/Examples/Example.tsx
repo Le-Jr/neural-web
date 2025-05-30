0655cdcf619612d815a8fa80206a8cc193a8e01b
Le-Jr <nascimentoleandrojr@gmail.com>
2025-05-30 10:37:19 -0300

    feat: fix cards mobile

--- src/Components/Examples/Example.tsx
+++ src/Components/Examples/Example.tsx
@@ -1,31 +1,60 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Examples = () => {
+  const cards = [
+    {
+      id: 1,
+      title: "Página de captura",
+      img: "mercurio.png",
+      link: "https://mercuriochat.com.br"
+    },
+    {
+      id: 2,
+      title: "Página de Vendas",
+      img: "example.png",
+      link: "https://mercuriochat.com.br"
+    }
+  ]
+
+
   return (
     <section className="container mx-auto px-4 max-w-2xl text-center mt-15 flex justify-between flex-col items-center">
       <h3 className="font-bold text-2xl">Exemplos do que você vai receber</h3>
-      <h5 className="text-muted-foreground text-md">Páginas profissionais para diferentes objetivos</h5>
-      <section className="flex gap-5">
-        <div className="w-75 shadow-lg bg-white flex flex-col items-center rounded-md p-4 gap-3 mt-6">
-          <a href="https://mercuriochat.com.br" target="_blank" className="block">
-            <img
-              src="example.png"
-              alt="Mercúrio Chat"
-              className="w-full h-32 object-cover rounded-md"
-            />
-          </a>
-          <p className="font-bold text-xl">página de captura</p>
-        </div>
-        <div className="w-75 shadow-lg bg-white flex flex-col items-center rounded-md p-4 gap-3 mt-4">
-          <a href="https://mercuriochat.com.br" target="_blank" className="block">
-            <img
-              src="mercurio.png"
-              alt="Mercúrio Chat"
-              className="w-full h-32 object-cover rounded-md"
-            />
-          </a>
-          <p className="font-bold text-xl">página de venda</p>
-        </div>
-      </section>
+      <h5 className="text-muted-foreground text-md">
+        Páginas profissionais para diferentes objetivos
+      </h5>
+      <Carousel
+        className="mt-3 w-9/12 max-w-xs"
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {cards.map((item) =>(
+            <CarouselItem key={item.id}>
+              <div className="p-1">
+                <Card className="shadow-2xs shadow-emerald-950">
+                  <CardContent className="flex flex-col items-center justify-center p-6 space-y-4">
+                    <a href={item.link} ><img src={item.img} alt={item.title} className="w-11/12 h-32 object-cover rounded-lg" /></a>
+                    <p>{item.title}</p>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    
     </section>
   );
 };
